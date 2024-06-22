595faf65c5df8f91a0d395224caa0b7a814d1ef9
em-mw <01350311max247@gmail.com>
2024-06-22 14:18:46 -0500

    checkpoint

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import mediapipe as mp
 import threading, gui
 from PyQt6 import QtCore, QtGui, QtWidgets
 import sys
@@ -5,13 +6,42 @@ import cv2
 from PyQt6.QtCore import QTimer
 from PyQt6.QtGui import QImage, QPixmap
 from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QVBoxLayout, QWidget
+    
 
 class frame:
-    def cam(self, window):
-        vid = cv2.VideoCapture(0)
+    def cam(self, window) -> None:
+        cam_no = int(window.CameraBox.value())
+        try:vid = cv2.VideoCapture(cam_no)
+        except:
+            print("error")
+            vid = cv2.VideoCapture(cam_no)
+        hand = mp.solutions.hands.Hands()
+        point = mp.solutions.drawing_utils
         while True:
-            ret, frame = vid.read()
+            try: 
+                if cam_no != int(window.CameraBox.value()): cam_no = int(window.CameraBox.value())
+            except: 
+                window.label_2.setText("❗📷➡️🧾❗SET CAM AS NUMBER NOT TEXT")
+            _, frame = vid.read()
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame_height, frame_width, _ = frame.shape
+            hand_output = hand.process(frame)
+            hands_disp = hand_output.multi_hand_landmarks
+            #print(hands_disp)
+            if hands_disp and window.MouseCamButton.isChecked():
+                for point_data in hands_disp:
+                    point.draw_landmarks(frame, point_data, mp.solutions.hands.HAND_CONNECTIONS)
+                    landmarks = point_data.landmark
+                    for id, landmark in enumerate(landmarks):
+                        x = int(landmark.x*frame_width)
+                        y = int(landmark.y*frame_height)
+                        print(f"{x}, {y}")
+                        if id == 8:
+                            window.label_2.setText(f"🟢 Status: Detection ({x}, {y})")
+                            cv2.circle(img=frame, center=(x,y), radius=12, color=(0,255,0))
+                        
+            else:window.label_2.setText("🟡 Status: Detection (pending hand input...)")
+
             h, w, ch = frame.shape
             bytes_per_line = ch * w
             #image = QImage(frame, frame.shape[1], frame.shape[0], frame.strides[0])
@@ -20,7 +50,6 @@ class frame:
             #self.image_label.setPixmap(pixmap)
             window.image_label.setPixmap(pixmap)
             window.image_label.setScaledContents(False)
-
             if dead: break
 
 if __name__ == "__main__":
@@ -34,7 +63,6 @@ if __name__ == "__main__":
     iframe = frame()
     x = threading.Thread(target=iframe.cam, args=(ui,))
     x.start()
-    #x.join()
     MainWindow.show()
     try:
         sys.exit(app.exec())
